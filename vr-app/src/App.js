871b92359a7726502ddf349f547a8968dad669c6
healingfields <dihratene@gmail.com>
2022-08-03 15:38:37 +0100

    images to use as a backgrounds

--- vr-app/src/App.js
+++ vr-app/src/App.js
@@ -1,12 +1,19 @@
 import { Canvas } from '@react-three/fiber'
+import { Suspense } from 'react';
 import './App.css';
 import Box from './components/Box';
+import Controls from './components/Control';
+import Panorama from './components/Panorama';
 
 
 function App() {
   return (
     <div id="canvas-container">
       <Canvas>
+        <Controls />
+        <Suspense fallback={null}>
+          <Panorama />
+        </Suspense>
         <Box />
       </Canvas>
     </div>
