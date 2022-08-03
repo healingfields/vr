ea151126e1d707194cb9243d1c52031f0f09fdeb
healingfields <dihratene@gmail.com>
2022-08-03 16:12:47 +0100

    added logic to change background on click

--- vr-app/src/components/Panorama.js
+++ vr-app/src/components/Panorama.js
@@ -1,5 +1,7 @@
 import { useLoader } from "@react-three/fiber"
+import { useState } from "react";
 import * as THREE from 'three'
+import Box from './Box'
 
 const backgrounds = [
     {
@@ -14,15 +16,27 @@ const backgrounds = [
 ]
 
 export default function Panorama() {
-    const background = useLoader(THREE.TextureLoader,
-        backgrounds[0].url);
+
+    const [activeBackground, setActiveBackground] = useState(1)
+
+    const { url } = backgrounds.find(({ id }) => id == activeBackground);
+    const background = useLoader(THREE.TextureLoader, url)
+
 
     return (
-        <mesh>
-            <sphereBufferGeometry args={[500, 60, 40]} />
-            <meshBasicMaterial map={background} side={THREE.BackSide} />
-
-        </mesh>
+        <group>
+            <mesh>
+                <sphereBufferGeometry args={[500, 60, 40]} />
+                <meshBasicMaterial map={background} side={THREE.BackSide} />
+            </mesh>
+            <group
+                onClick={(e) => {
+                    e.stopPropagation();
+                    setActiveBackground(activeBackground === 1 ? 2 : 1);
+                }}>
+                <Box />
+            </group>
+        </group>
     )
 }
 
